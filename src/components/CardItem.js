22f8e291766cc7195f630f1721a09be342a1ed57
Sworzen1 <b.sworzen@gmail.com>
2020-05-17 23:26:41 +0200

    Change CardItem component

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,57 +1,92 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-import cake from "../images/cake.jpg";
+import React, { useState } from "react";
+import { makeStyles} from "@material-ui/core/styles";
+import Dialog from '@material-ui/core/Dialog';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
 
 const CardItem = (props) => {
   const classes = useStyles();
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={cake}
-        />
+  const [hover, setHover] = useState(false);
+  const [open, setOpen] = useState(false);
 
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="h2"
-            className={classes.cardTitle}
-          >
-            {props.title}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+  const handleOpen = () => {
+    setOpen(true);
+    setHover(false)
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${props.image})` }}
+      className={classes.media}
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+    >
+      {hover == true ? (
+        <h2 className={classes.cardTitleHover} onClick={handleOpen}>
+          View more
+        </h2>
+      ) : (
+        <h2 className={classes.cardTitle} onMouseOver={() => setHover(true)} >{props.title}</h2>
+      )}
+      
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >        <IconButton aria-label="close" className={classes.closeButton} onClick={handleClose}>
+      <CloseIcon />
+    </IconButton>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to
+            Google, even when no apps are running.
+          </DialogContentText>
+      </Dialog>
+    </div>
   );
 };
 const useStyles = makeStyles({
-  root: {
-    width: 300,
-    height: 370,
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
-    border: 'none',
-    margin: 'auto',
-    '&:hover': {
-      boxShadow: '10px 10px 5px 0px rgba(0,0,0,0.45)'
-    }
-  },
   media: {
-    height: 310
+    height: "100%",
+    backgroundSize: "cover",
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center",
   },
   cardTitle: {
-    color: '#c5a645',
-    fontSize: '20px',
-    fontWeight: 800,
-    textAlign: 'center'
-  }
+    color: "white",
+    fontSize: "28px",
+    fontWeight: 100,
+    backgroundColor: "#c5a645",
+    width: "80%",
+    height: "20%",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "80%",
+    transition: "1s",
+  },
+  cardTitleHover: {
+    color: "black",
+    fontSize: "28px",
+    fontWeight: 100,
+    backgroundColor: "white",
+    width: "80%",
+    height: "20%",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "80%",
+    transition: "1s",
+    cursor: "pointer",
+  },
 });
 
 export default CardItem;
