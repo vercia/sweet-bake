300e8c8c16649d09c7e762049cc6f13ab860080c
Weronika <werawera112@gmail.com>
2020-05-19 19:35:17 +0200

    Updates

---
+++ precache-manifest.ac98cffd3540d394ce95de9044738aa7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7130418bbddd534b45c5f1eb623acd6",
+    "url": "/sweet-bake/index.html"
+  },
+  {
+    "revision": "e7afc0d6beff98042b9b",
+    "url": "/sweet-bake/static/css/2.ed90cac3.chunk.css"
+  },
+  {
+    "revision": "0f543ac164cf2813d0bc",
+    "url": "/sweet-bake/static/css/main.fcb113a3.chunk.css"
+  },
+  {
+    "revision": "e7afc0d6beff98042b9b",
+    "url": "/sweet-bake/static/js/2.57fd22a6.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/sweet-bake/static/js/2.57fd22a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f543ac164cf2813d0bc",
+    "url": "/sweet-bake/static/js/main.b7290f15.chunk.js"
+  },
+  {
+    "revision": "dbed00e213a1c1b16d74",
+    "url": "/sweet-bake/static/js/runtime-main.96f4b0bf.js"
+  },
+  {
+    "revision": "377564a390d9c6104762b3aeae4d57dc",
+    "url": "/sweet-bake/static/media/background.377564a3.jpg"
+  },
+  {
+    "revision": "ca77f49f77dd298ad6ede7a0a4b1e4e9",
+    "url": "/sweet-bake/static/media/background1.ca77f49f.jpg"
+  },
+  {
+    "revision": "22aeca87c04339766fb4feac9a388154",
+    "url": "/sweet-bake/static/media/background3.22aeca87.jpg"
+  },
+  {
+    "revision": "7498499c18450666cc40b32116c2d2ec",
+    "url": "/sweet-bake/static/media/bake1.7498499c.jpg"
+  },
+  {
+    "revision": "99beccb737e6f83bfa1c9f232d4a3486",
+    "url": "/sweet-bake/static/media/bake10.99beccb7.jpg"
+  },
+  {
+    "revision": "526f4be8c6105f0f404c11f23a8d08b9",
+    "url": "/sweet-bake/static/media/bake2.526f4be8.jpg"
+  },
+  {
+    "revision": "4d29832b23133bc210feb59c076a15d1",
+    "url": "/sweet-bake/static/media/bake3.4d29832b.jpg"
+  },
+  {
+    "revision": "d31663482ac471edb7aa86cac370cc82",
+    "url": "/sweet-bake/static/media/bake4.d3166348.jpg"
+  },
+  {
+    "revision": "64b87ba4cd665a15315ba127bdc13326",
+    "url": "/sweet-bake/static/media/bake5.64b87ba4.jpg"
+  },
+  {
+    "revision": "ffb9edb17a13c5dab8815003bdb55e73",
+    "url": "/sweet-bake/static/media/bake6.ffb9edb1.jpg"
+  },
+  {
+    "revision": "9080acb9a9bdab84a93c0f9d6fc293f2",
+    "url": "/sweet-bake/static/media/bake7.9080acb9.jpg"
+  },
+  {
+    "revision": "7ace2e44390ca3df2c23cf6d67809581",
+    "url": "/sweet-bake/static/media/bake8.7ace2e44.jpg"
+  },
+  {
+    "revision": "26c881f0562c8ef5431d2af3592681b2",
+    "url": "/sweet-bake/static/media/bake9.26c881f0.jpg"
+  }
+]);
